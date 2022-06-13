ffab57154d596792944e819be6a46594600da68a
Melyn Atieno Were <melynatieno@gmail.com>
2022-06-13 14:55:01 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards( names, event ) {
+    let thanks = []
+    for ( let i = 0; i < names.length; i++ ) {
+      thanks.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+    return thanks
+  }
+  
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
